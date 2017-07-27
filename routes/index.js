1b981cbe57f6728cd88d2e886ab31ddd9a7e592a
DJ Versoza <djversoza@gmail.com>
2017-07-27 13:40:15 -0700

    fixing styling

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/main', (req, res, next) =>{
+  res.render('page')
+})
+
+
 
 module.exports = router;
